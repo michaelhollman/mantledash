eeaef6660a3ba90dde54d28c0fd6dc72e1080d94
Michael Hollman <hollyfrass@gmail.com>
2014-11-11 21:04:21 -0600

    essentially empty project

---
+++ index.js
@@ -0,0 +1,21 @@
+var fs = require('fs');
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+
+app.set('port', (process.env.PORT || 5000));
+app.use(express.static(__dirname + '/static'));
+
+app.get('/', function(request, response) {
+  fs.readFile('index.html', encoding='utf8', function (err, data) {
+    if (err) {
+      response.status(500).send(err);
+    } else {
+      response.send(data);
+    }
+  });
+});
+
+app.listen(app.get('port'), function() {
+  console.log('Node app is running at localhost:' + app.get('port'));
+});
